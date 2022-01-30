49dd34af07bd0f2909a2cabe6e585ee7b6bcb73c
Dani1324 <danieleronchini2@gmail.com>
2022-01-30 20:03:53 +0100

    1.01
    First upload online of the website

---
+++ main.js
@@ -0,0 +1,69 @@
+function calcolo() {
+  var corrette = 0;
+  var errate = 0;
+
+  if(document.getElementById('Q01A3').checked) {
+     corrette = corrette+1;
+   }
+  else{
+    errate = errate+1
+  }
+  if(document.getElementById('Q02A1').checked) {
+     corrette = corrette+1;
+   }
+  else{
+    errate = errate+1
+  }
+  if(document.getElementById('Q03A3').checked) {
+     corrette = corrette+1;
+   }
+  else{
+    errate = errate+1
+  }
+  if(document.getElementById('Q04A2').checked) {
+     corrette = corrette+1;
+   }
+  else{
+    errate = errate+1
+  }
+  if(document.getElementById('Q05A3').checked) {
+     corrette = corrette+1;
+   }
+  else{
+    errate = errate+1
+  }
+  if(document.getElementById('Q06A3').checked) {
+     corrette = corrette+1;
+   }
+  else{
+    errate = errate+1
+  }
+  if(document.getElementById('Q07A2').checked) {
+     corrette = corrette+1;
+   }
+  else{
+    errate = errate+1
+  }
+  if(document.getElementById('Q08A3').checked) {
+     corrette = corrette+1;
+   }
+  else{
+    errate = errate+1
+  }
+  if(document.getElementById('Q09A1').checked) {
+     corrette = corrette+1;
+   }
+  else{
+    errate = errate+1
+  }
+  if(document.getElementById('Q10A3').checked) {
+     corrette = corrette+1;
+   }
+  else{
+    errate = errate+1
+  }
+
+  document.getElementById("a-true").innerHTML = corrette;
+  document.getElementById("a-false").innerHTML = errate;
+  document.getElementById("a-percentage").innerHTML = corrette/10*100+"%";
+}

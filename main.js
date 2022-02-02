8cff007dc112f7d99ffe7d59ae5553c00fd0f5d1
Dani1324 <danieleronchini2@gmail.com>
2022-02-02 15:22:26 +0100

    1.05

--- main.js
+++ main.js
@@ -2,68 +2,80 @@ function calcolo() {
   var corrette = 0;
   var errate = 0;
 
+
   if(document.getElementById('Q01A3').checked) {
-     corrette = corrette+1;
+    corrette = corrette+1;
    }
   else{
     errate = errate+1
   }
   if(document.getElementById('Q02A1').checked) {
-     corrette = corrette+1;
+    corrette = corrette+1;
    }
   else{
     errate = errate+1
   }
   if(document.getElementById('Q03A3').checked) {
-     corrette = corrette+1;
+    corrette = corrette+1;
    }
   else{
     errate = errate+1
   }
   if(document.getElementById('Q04A2').checked) {
-     corrette = corrette+1;
+    corrette = corrette+1;
    }
   else{
     errate = errate+1
   }
   if(document.getElementById('Q05A3').checked) {
-     corrette = corrette+1;
+    corrette = corrette+1;
    }
   else{
     errate = errate+1
   }
   if(document.getElementById('Q06A3').checked) {
-     corrette = corrette+1;
+    corrette = corrette+1;
    }
   else{
     errate = errate+1
   }
   if(document.getElementById('Q07A2').checked) {
-     corrette = corrette+1;
+    corrette = corrette+1;
    }
   else{
     errate = errate+1
   }
   if(document.getElementById('Q08A3').checked) {
-     corrette = corrette+1;
+    corrette = corrette+1;
    }
   else{
     errate = errate+1
   }
   if(document.getElementById('Q09A1').checked) {
-     corrette = corrette+1;
+    corrette = corrette+1;
    }
   else{
     errate = errate+1
   }
   if(document.getElementById('Q10A3').checked) {
-     corrette = corrette+1;
+    corrette = corrette+1;
    }
   else{
     errate = errate+1
   }
 
+  if(errate>5) {
+    $("#show-1").hide();
+    $("#hide-2").hide();
+   }
+  else{
+    $("#neutral-animation").hide();
+    $("#happy-animation").hide();
+  }
+
   document.getElementById("a-true").innerHTML = corrette;
   document.getElementById("a-false").innerHTML = errate;
   document.getElementById("a-percentage").innerHTML = corrette/10*100+"%";
+
+
 }
